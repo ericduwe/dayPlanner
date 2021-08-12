5cb7ea77f5714b9979b10545a7aae7eb27ed71f9
Eric Duwe <ericduwe@gmail.com>
2021-08-12 16:33:59 -0500

    added js conditionals for current time to each row

---
+++ assets/script.js
@@ -0,0 +1,40 @@
+var timeBlocks = document.getElementsByClassName("time-block");
+currentTime = moment().hour()
+
+Array.from(timeBlocks).forEach(block => {
+    let blockId = block.id,
+    blockHour;
+    if (blockId) {
+        blockHour = parseInt(blockId);
+    }
+    if (blockHour) {
+        if (currentTime === blockHour) {
+            setBackground(block, "red")
+        } else if (currentTime < blockHour) {
+            setBackground(block, "green")
+        } else {
+            setBackground(block, "lightgrey")
+        }
+    }
+
+});
+
+
+
+function setBackground(element, color) {
+    element.style.backgroundColor = color;
+}
+
+// function checkTime() {
+
+//     //or (i = 0; i < 24; i++) {
+
+//     if (currentTime === hour) {
+//         document.getElementsByClassName("time-block").style.
+//         } else if (currentTime < hour) {
+//         timeBlock.classList.remove("future");
+//     }
+// }
+// //}
+
+// checkTime()
