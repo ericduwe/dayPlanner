a70812087d0f749820ecd5ee4d4bfd5c5e1274cc
Eric Duwe <ericduwe@gmail.com>
2021-08-12 18:45:52 -0500

    added event listeners to save buttons

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 var timeBlocks = document.getElementsByClassName("time-block");
 var currentDay = document.getElementById("currentDay");
+var eventText = document.getElementsByClassName("description").value
+var saveButton = document.getElementsByClassName("saveBtn")
 currentTime = moment().hour()
 
 currentDay.textContent = moment().format("dddd, MMM Do YYYY")
@@ -22,8 +24,27 @@ Array.from(timeBlocks).forEach(block => {
 
 });
 
+for (var i=0; i < saveButton.length; i++) {
+    var buttons = saveButton[i]
+    buttons.addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log("Clicked the button!")
+    })
+}
+
+function saveAppointment () {
+    
+    Array.from(eventText).forEach(element => {
+    if (element === "") {
+        alert("Error: Description cannot be blank.");
+    } else {
+        alert(`${element} saved!`);
+    }
+    });
+}
+
+
 function changeBlock(element, color) {
     element.style.backgroundColor = color;
     
-}
-
+};
