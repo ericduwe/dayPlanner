b908d2fd284b3fdbe7582ab683cfcf2a91113dee
Eric Duwe <ericduwe@gmail.com>
2021-08-12 22:40:26 -0500

    more changes to localStorage function

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ var timeBlocks = document.getElementsByClassName("time-block");
 var currentDay = document.getElementById("currentDay");
 var eventText = document.getElementsByClassName("description")
 var saveButton = document.getElementsByClassName("saveBtn")
+
 currentTime = moment().hour()
 
 currentDay.textContent = moment().format("dddd, MMM Do YYYY")
@@ -25,22 +26,19 @@ Array.from(timeBlocks).forEach(block => {
 
 });
 
+function saveAppointment() {
 for (var i = 0; i < saveButton.length; i++) {
     var buttons = saveButton[i]
     buttons.addEventListener("click", function (event) {
+        for (var i=0; i<eventText.length; i++) {
         event.preventDefault();
-        console.log("Clicked the button!")
-        saveAppointment();
+        var eventTextArea = eventText[i];
+        console.log(eventTextArea.value);
+        localStorage.setItem("Appointment", JSON.stringify(eventTextArea.value));
+        }
     })
 };
 
-function saveAppointment() {
-
-    for (var i = 0; i < eventText.length; i++) {
-        //var eventContent = eventText.value
-        var singleEventText = eventText[i].value
-        localStorage.setItem("Appointment", JSON.stringify(singleEventText))
-    }
 };
 
 
@@ -54,3 +52,5 @@ function changeBlock(element, color) {
     element.style.backgroundColor = color;
 
 };
+
+saveAppointment();
